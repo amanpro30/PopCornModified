315463e4701837f4186f83cb79c6eb78756f9fdd
amanpro30 <aman.k17@iiits.in>
2020-10-25 22:03:05 +0530

    Changes in settings

--- PopCorn/settings.py
+++ PopCorn/settings.py
@@ -148,6 +148,8 @@ STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'popcornproject123@gmail.com'
